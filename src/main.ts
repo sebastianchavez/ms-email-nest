2e9535e607e0363ab7a6826caf65ac4bc088a7c1
Sebastián <seba.chavez21@gmail.com>
2024-03-24 12:19:17 -0300

    ms ok

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+  await app.listen(process.env.PORT);
+}
+bootstrap();
